5939858c4aa79472512e812333e8d6b7f3b014a2
donggeon <dkms625@naver.com>
2025-03-24 17:26:18 +0900

    create helloSpringMVC

---
+++ src/main/java/kr/ac/hansung/cse/controller/OfferController.java
@@ -0,0 +1,26 @@
+package kr.ac.hansung.cse.controller;
+
+import kr.ac.hansung.cse.model.Offer;
+import kr.ac.hansung.cse.service.OfferService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class OfferController {
+    //Controller -> service -> Dao
+    @Autowired
+    private OfferService offerService;
+
+    @GetMapping("/offers")
+    public String showOffers(Model model) {
+        List<Offer> offers = offerService.getAllOffers();  // 비즈니스 로직 수행
+        model.addAttribute("id_offers", offers);  // 모델에 넣음
+
+        return "offers";
+    }
+
+}
